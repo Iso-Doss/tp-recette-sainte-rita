49f3eb1386a20b1ce37b3d1a9c9633376c3b9793
Iso-Doss <dosou.israel48@gmail.com>
2024-03-02 11:44:35 +0100

    Validation des champs nom de famille, prenoms, sexe pour le formulaire d'inscription

--- app/inscription/index.php
+++ app/inscription/index.php
@@ -1,9 +1,55 @@
 <!-- inscription.php -->
+<?php
+// Le message de success global pour l'inscription.
+$message = "";
+// Le message d'erreur global pour l'inscription.
+$erreur = "";
+// Les données envoyées depuis le formulaire d'inscription.
+$donnees = [];
+// Les messages d'erreur spécifique a chaque champ incorrect du formulaire d'inscription.
+$erreurs = [];
+
+if (isset($_GET['message']) && !empty($_GET['message'])) {
+    $message = $_GET['message'];
+}
+
+if (isset($_GET['erreur']) && !empty($_GET['erreur'])) {
+    $erreur = $_GET['erreur'];
+}
+
+if (isset($_GET['donnees']) && !empty($_GET['donnees'])) {
+    $donnees = json_decode($_GET['donnees'], true);
+}
+
+if (isset($_GET['erreurs']) && !empty($_GET['erreurs'])) {
+    $erreurs = json_decode($_GET['erreurs'], true);
+}
+
+?>
 
 <div class="row mt-3 d-flex justify-content-center align-items-center">
     <div class="col-md-6">
         <h1 class="text-center">S'inscrire</h1>
-        <form action="index.php?page=inscription-traitement" method="post">
+
+        <?php
+        if (!empty($message)) {
+        ?>
+            <div class="alert alert-success" role="alert">
+                <?= $message; ?>
+            </div>
+        <?php
+        }
+
+        if (!empty($erreur)) {
+        ?>
+            <div class="alert alert-danger" role="alert">
+                <?= $erreur; ?>
+            </div>
+        <?php
+        }
+        ?>
+
+        <form action="index.php?page=inscription-traitement" method="post" novalidate>
             <div class="mb-3">
                 <label for="inscription-nom" class="form-label">
                     Nom de famille :
@@ -11,7 +57,10 @@
                         (*)
                     </span>
                 </label>
-                <input type="text" name="nom" class="form-control inscription-nom" id="inscription-nom" placeholder="Veuillez entrer votre nom de famille">
+                <input type="text" name="nom" class="form-control inscription-nom" id="inscription-nom" placeholder="Veuillez entrer votre nom de famille" value="<?= (isset($donnees['nom']) && !empty($donnees['nom'])) ? $donnees['nom'] : ''; ?>" required>
+                <p class="text-danger">
+                    <?= (isset($erreurs['nom']) && !empty($erreurs['nom'])) ? $erreurs['nom'] : ''; ?>
+                </p>
             </div>
 
             <div class="mb-3">
@@ -21,7 +70,10 @@
                         (*)
                     </span>
                 </label>
-                <input type="text" name="prenoms" class="form-control inscription-prenoms" id="inscription-prenoms" placeholder="Veuillez entrer vos prénoms">
+                <input type="text" name="prenoms" class="form-control inscription-prenoms" id="inscription-prenoms" placeholder="Veuillez entrer vos prénoms" value="<?= (isset($donnees['prenoms']) && !empty($donnees['prenoms'])) ? $donnees['prenoms'] : ''; ?>" required>
+                <p class="text-danger">
+                    <?= (isset($erreurs['prenoms']) && !empty($erreurs['prenoms'])) ? $erreurs['prenoms'] : ''; ?>
+                </p>
             </div>
 
             <div class="mb-3">
@@ -31,12 +83,15 @@
                         (*)
                     </span>
                 </label>
-                <select class="form-select" id="inscription-sexe" name="sexe">
-                    <option selected>Veuillez sélectionner votre sexe</option>
-                    <option value="M">Homme</option>
-                    <option value="F">Femme</option>
-                    <option value="A">Autres</option>
+                <select class="form-select" id="inscription-sexe" name="sexe" required>
+                    <option <?= (isset($donnees['sexe']) && !empty($donnees['sexe']) && $donnees['sexe'] == '') ? 'selected' : '' ?>>Veuillez sélectionner votre sexe</option>
+                    <option <?= (isset($donnees['sexe']) && !empty($donnees['sexe']) && $donnees['sexe'] == 'M') ? 'selected' : '' ?> value="M">Homme</option>
+                    <option <?= (isset($donnees['sexe']) && !empty($donnees['sexe']) && $donnees['sexe'] == 'F') ? 'selected' : '' ?> value="F">Femme</option>
+                    <option <?= (isset($donnees['sexe']) && !empty($donnees['sexe']) && $donnees['sexe'] == 'A') ? 'selected' : '' ?> value="A">Autres</option>
                 </select>
+                <p class="text-danger">
+                    <?= (isset($erreurs['sexe']) && !empty($erreurs['sexe'])) ? $erreurs['sexe'] : ''; ?>
+                </p>
             </div>
 
             <div class="mb-3">
@@ -46,7 +101,7 @@
                         (*)
                     </span>
                 </label>
-                <input type="email" name="email" class="form-control inscription-email" id="inscription-email" placeholder="Veuillez entrer votre adresse mail">
+                <input type="email" name="email" class="form-control inscription-email" id="inscription-email" placeholder="Veuillez entrer votre adresse mail" required>
             </div>
 
             <div class="mb-3">
@@ -56,7 +111,7 @@
                         (*)
                     </span>
                 </label>
-                <input type="password" name="mot-de-passe" class="form-control inscription-mot-de-passe" id="inscription-mot-de-passe" placeholder="Veuillez entrer votre mot de passe">
+                <input type="password" name="mot-de-passe" class="form-control inscription-mot-de-passe" id="inscription-mot-de-passe" placeholder="Veuillez entrer votre mot de passe" required>
             </div>
 
             <div class="mb-3">
@@ -66,19 +121,17 @@
                         (*)
                     </span>
                 </label>
-                <input type="password" name="confirmation-mot-de-passe" class="form-control inscription-confirmation-mot-de-passe" id="inscription-confirmation-mot-de-passe" placeholder="Veuillez entrer confirmer votre mot de passe">
+                <input type="password" name="confirmation-mot-de-passe" class="form-control inscription-confirmation-mot-de-passe" id="inscription-confirmation-mot-de-passe" placeholder="Veuillez entrer confirmer votre mot de passe" required>
             </div>
 
             <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="inscription-terms-conditions">
+                <input type="checkbox" class="form-check-input" id="inscription-terms-conditions" required>
                 <label class="form-check-label" for="inscription-terms-conditions">Oui j'accepte les terms et conditions du site de recettes</label>
             </div>
 
             <div class="mb-3">
                 <button type="submit" class="btn btn-primary">S'inscrire</button>
             </div>
-
-
 
             <div class="mb-3 text-center">
                 <a href="index.php?page=connexion">J'ai déja un compte</a>
--- app/inscription/traitement.php
+++ app/inscription/traitement.php
@@ -1,3 +1,37 @@
 <!-- inscription-traitement.php -->
-<?php 
+<?php
+// Le message de success global pour l'inscription.
+$message = "";
+// Le message d'erreur global pour l'inscription.
+$erreur = "";
+// Les données envoyées depuis le formulaire d'inscription.
+$donnees = $_POST;
+// Les messages d'erreur spécifique a chaque champ incorrect du formulaire d'inscription.
+$erreurs = [];
 
+if (empty($_POST)) {
+    $erreur = "Oupss!!! Un ou plusieur(s) champs sont incorrect";
+} else {
+    //$message = "Tous est Ok";
+    if (!isset($_POST['nom']) || empty($_POST['nom'])) {
+        $erreurs['nom'] = "Le champ nom de famille est vide ou inccorect.";
+    }
+
+    if (!isset($_POST['prenoms']) || empty($_POST['prenoms'])) {
+        $erreurs['prenoms'] = "Le champ prénoms est vide ou inccorect.";
+    }
+
+    if (
+        !isset($_POST['sexe']) ||
+        empty($_POST['sexe']) ||
+        ($_POST['sexe'] !== 'A' && $_POST['sexe'] !== 'F' && $_POST['sexe'] !== 'M')
+    ) {
+        $erreurs['sexe'] = "Le champ sexe est vide ou inccorect.";
+    }
+
+    if (!empty($erreurs)) {
+        $erreur = "Oupss!!! Un ou plusieur(s) champs sont incorrect";
+    }
+}
+
+header("location:index.php?page=inscription&message=" . $message . "&erreur=" . $erreur . "&donnees=" . json_encode($donnees) . "&erreurs=" . json_encode($erreurs));
